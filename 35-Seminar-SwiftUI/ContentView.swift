67b652e6b56e9cc62c7923d9f8ad9f2386363c49
최지석 <jiseok212@gmail.com>
2024-11-23 12:21:33 +0900

    [Chore] Seminar-35 프로젝트 내 SwiftUI Target 추가

---
+++ 35-Seminar-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  35-Seminar-SwiftUI
+//
+//  Created by 최지석 on 11/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 35-Seminar-SwiftUI/SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIApp.swift
+//  35-Seminar-SwiftUI
+//
+//  Created by 최지석 on 11/23/24.
+//
+
+import SwiftUI
+
+@main
+struct _5_Seminar_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
